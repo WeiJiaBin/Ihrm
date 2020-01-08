d8de5ee8ebf2baa2f067abee1451af599d4a4ed7
bin <793048472@qq.com>
2020-01-08 20:33:57 +0800

    poi导出和导出模块导入,有空处理一下全局异常处理类

--- ihrm_parent/ihrm_employee/src/main/java/com/ihrm/employee/controller/EmployeeController.java
+++ ihrm_parent/ihrm_employee/src/main/java/com/ihrm/employee/controller/EmployeeController.java
@@ -386,6 +386,102 @@ public class EmployeeController extends BaseController {
 //        wb.write(os);
 //        new DownloadUtils().download(os,response,month+"人事报表.xlsx");
 //    }
+
+
+
+        @RequestMapping(value = "/export", method = RequestMethod.GET)
+    public void export1(@PathVariable String month) throws Exception {
+        //1.获取报表数据
+        List<EmployeeReportResult> list = userCompanyPersonalService.findByReport(companyId,month);
+
+        //2.加载模板
+        Resource resource = new ClassPathResource("excel-template/hr-demo.xlsx");
+        FileInputStream fis = new FileInputStream(resource.getFile());
+
+        //3.通过工具类完成下载
+//        new ExcelExportUtil(EmployeeReportResult.class,2,2).
+//                export(response,fis,list,month+"人事报表.xlsx");
+
+
+        //3.根据模板创建工作簿
+        Workbook wb = new XSSFWorkbook(fis);
+        //4.读取工作表
+        Sheet sheet = wb.getSheetAt(0);
+        //5.抽取公共样式
+        Row row = sheet.getRow(2);
+        CellStyle styles [] = new CellStyle[row.getLastCellNum()];
+        for(int i=0;i<row.getLastCellNum();i++) {
+            Cell cell = row.getCell(i);
+            styles[i] = cell.getCellStyle();
+        }
+        //6.构造单元格
+        int rowIndex = 2;
+        Cell cell=null;
+        for(int i=0;i<10000;i++) {
+            for (EmployeeReportResult employeeReportResult : list) {
+                row = sheet.createRow(rowIndex++);
+                // 编号,
+                cell = row.createCell(0);
+                cell.setCellValue(employeeReportResult.getUserId());
+                cell.setCellStyle(styles[0]);
+                // 姓名,
+                cell = row.createCell(1);
+                cell.setCellValue(employeeReportResult.getUsername());
+                cell.setCellStyle(styles[1]);
+                // 手机,
+                cell = row.createCell(2);
+                cell.setCellValue(employeeReportResult.getMobile());
+                cell.setCellStyle(styles[2]);
+                // 最高学历,
+                cell = row.createCell(3);
+                cell.setCellValue(employeeReportResult.getTheHighestDegreeOfEducation());
+                cell.setCellStyle(styles[3]);
+                // 国家地区,
+                cell = row.createCell(4);
+                cell.setCellValue(employeeReportResult.getNationalArea());
+                cell.setCellStyle(styles[4]);
+                // 护照号,
+                cell = row.createCell(5);
+                cell.setCellValue(employeeReportResult.getPassportNo());
+                cell.setCellStyle(styles[5]);
+                // 籍贯,
+                cell = row.createCell(6);
+                cell.setCellValue(employeeReportResult.getNativePlace());
+                cell.setCellStyle(styles[6]);
+                // 生日,
+                cell = row.createCell(7);
+                cell.setCellValue(employeeReportResult.getBirthday());
+                cell.setCellStyle(styles[7]);
+                // 属相,
+                cell = row.createCell(8);
+                cell.setCellValue(employeeReportResult.getZodiac());
+                cell.setCellStyle(styles[8]);
+                // 入职时间,
+                cell = row.createCell(9);
+                cell.setCellValue(employeeReportResult.getTimeOfEntry());
+                cell.setCellStyle(styles[9]);
+                // 离职类型,
+                cell = row.createCell(10);
+                cell.setCellValue(employeeReportResult.getTypeOfTurnover());
+                cell.setCellStyle(styles[10]);
+                // 离职原因,
+                cell = row.createCell(11);
+                cell.setCellValue(employeeReportResult.getReasonsForLeaving());
+                cell.setCellStyle(styles[11]);
+                // 离职时间
+                cell = row.createCell(12);
+                cell.setCellValue(employeeReportResult.getResignationTime());
+                cell.setCellStyle(styles[12]);
+            }
+        }
+        //7.下载
+        //3.完成下载
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        wb.write(os);
+        new DownloadUtils().download(os,response,month+"人事报表.xlsx");
+    }
+
+
 }
 
 
